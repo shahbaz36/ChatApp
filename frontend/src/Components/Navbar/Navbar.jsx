a98bb11a0e39c56c46cd303365d4535001a0d4c1
Shahbaz Ali <aquibahmad345@gmail.com>
2024-10-28 19:10:04 +0530

    Add hook to search for users based on data coming from search input

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,17 @@
 import styles from "./Navbar.module.css";
 
-import { useEffect, useState } from "react";
-import { useCookies } from "react-cookie";
-import axios from "axios";
-
 import find from "../../assets/find.png";
 import Notif from "../../assets/Notif.png";
 import user from "../../assets/user.png";
 
 import Sidedrawer from "../Sidedrawer/Sidedrawer";
+import { useSearchUser } from "../../hooks/useSearchUser";
+import { useState } from "react";
 
 function Navbar() {
   const [drawer, setDrawer] = useState(false);
   const [searchData, setSearchData] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
-  const [userData, setUserData] = useState();
-
-  const [cookies] = useCookies(["jwt"]);
+  const [isLoading, error, userData] = useSearchUser(searchData);
 
   const handleDrawerOpen = () => {
     setDrawer(true);
@@ -29,45 +23,6 @@ function Navbar() {
   const handleSearchUser = (e) => {
     setSearchData(e.target.value);
   };
-
-  useEffect(
-    function () {
-      async function fetchUserData() {
-        try {
-          setIsLoading(true);
-
-          const token = cookies.jwt;
-
-          if (!token) throw new Error("Unauthorized");
-
-          const config = {
-            headers: {
-              "Content-type": "application/json",
-              authorization: `Bearer ${token}`,
-            },
-            params: { search: searchData },
-          };
-
-          const response = await axios.get("/api/v1/users", config);
-
-          if (response.data.data.foundUsers.length === 0) {
-            throw new Error(`No users found!`);
-          }
-
-          setUserData(response.data.data.foundUsers);
-          console.log(userData);
-        } catch (error) {
-          if (error.message === "No users found!") setError(error.message);
-          console.log(error);
-        } finally {
-          setIsLoading(false);
-          setError("");
-        }
-      }
-      fetchUserData();
-    },
-    [searchData, cookies.jwt]
-  );
 
   return (
     <nav>
@@ -93,6 +48,7 @@ function Navbar() {
         error={error}
         isLoading={isLoading}
         userData={userData}
+        setDrawer={setDrawer}
       />
     </nav>
   );
---
+++ frontend/src/hooks/useSearchUser.jsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+
+export const useSearchUser = (searchData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [userData, setUserData] = useState([]);
+
+  const [cookies] = useCookies(["jwt"]);
+  useEffect(
+    function () {
+      async function fetchSearchUserData() {
+        try {
+          setIsLoading(true);
+          setUserData([]);
+          const token = cookies.jwt;
+
+          if (!token) throw new Error("Unauthorized");
+
+          const config = {
+            headers: {
+              "Content-type": "application/json",
+              authorization: `Bearer ${token}`,
+            },
+            params: { search: searchData },
+          };
+
+          const response = await axios.get("/api/v1/users", config);
+
+          if (response.data.data.foundUsers.length === 0) {
+            throw new Error(`No users found!`);
+          }
+
+          setUserData(response.data.data.foundUsers);
+          console.log(userData);
+        } catch (error) {
+          if (error.message === "No users found!") setError(error.message);
+          setUserData([]);
+        } finally {
+          setIsLoading(false);
+          setError("");
+        }
+      }
+      fetchSearchUserData();
+    },
+    [searchData, cookies.jwt]
+  );
+
+  return [isLoading, error, userData];
+};
