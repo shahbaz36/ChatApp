4a04ca50e1ba6025f032024df72b1ddd411f730e
Shahbaz Ali <aquibahmad345@gmail.com>
2024-10-31 04:09:16 +0530

    Add UI for user profile

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,16 +2,20 @@ import styles from "./Navbar.module.css";
 
 import find from "../../assets/find.png";
 import Notif from "../../assets/Notif.png";
-import user from "../../assets/user.png";
 
 import Sidedrawer from "../Sidedrawer/Sidedrawer";
 import { useSearchUser } from "../../hooks/useSearchUser";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { ChatContext } from "../../Context/ChatContext";
+import { X } from "lucide-react";
 
 function Navbar() {
+  const { user } = useContext(ChatContext);
   const [drawer, setDrawer] = useState(false);
   const [searchData, setSearchData] = useState("");
   const [isLoading, error, userData] = useSearchUser(searchData);
+
+  const [showProfile, setShowProfile] = useState(false);
 
   const handleDrawerOpen = () => {
     setDrawer(true);
@@ -39,9 +43,15 @@ function Navbar() {
         <div className={styles.navMid}>Talks-App</div>
         <div className={styles.navRight}>
           <img src={Notif} alt="" />
-          <img src={user} alt="" />
+          <img
+            src={user.pic}
+            id={styles.userImg}
+            alt="User_image"
+            onClick={() => setShowProfile(true)}
+          />
         </div>
       </div>
+      {showProfile && <Profile user={user} setShowProfile={setShowProfile} />}
       <Sidedrawer
         onDrawerClose={handleDrawerClose}
         drawer={drawer}
@@ -51,6 +61,28 @@ function Navbar() {
         setDrawer={setDrawer}
       />
     </nav>
+  );
+}
+
+function Profile({ user, setShowProfile }) {
+  return (
+    <>
+      <div className={styles.protectOverlay}></div>
+      <div className={styles.profile}>
+        <div className={styles.profileHead}>
+          <h2>{user.name}</h2>
+          <button>
+            <X size={18} onClick={() => setShowProfile(false)} />
+          </button>
+        </div>
+        {user && (
+          <div className={styles.profileContainer}>
+            <img src={user.pic} alt="" />
+            <h2>Email : {user.email}</h2>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
