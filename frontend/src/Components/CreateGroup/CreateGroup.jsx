f5f8974ccca168499dc53f0b8c71f4854ca1f6e4
Shahbaz Ali <aquibahmad345@gmail.com>
2024-10-31 20:21:35 +0530

    Refactoring

---
+++ frontend/src/Components/CreateGroup/CreateGroup.jsx
@@ -0,0 +1,129 @@
+import { useState } from "react";
+import styles from "./CreateGroup.module.css";
+import { useSearchUser } from "../../hooks/useSearchUser";
+import { X } from "lucide-react";
+
+function CreateGroup({ setIsVisible }) {
+  const [chatName, setChatName] = useState("");
+  const [search, setSearch] = useState("");
+  const [isLoading, error, userData] = useSearchUser(search);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+
+  const handleSearchUser = (e) => {
+    setSearch(e.target.value);
+    console.log(userData);
+  };
+
+  const handleChatName = (e) => {
+    setChatName(e.target.value);
+  };
+
+  const handleAddToSelected = (user) => {
+    let isUserAlreadyAdded = false;
+    selectedUsers.forEach((addedMember) => {
+      if (addedMember.email === user.email) {
+        isUserAlreadyAdded = true;
+      }
+    });
+    if (!isUserAlreadyAdded) {
+      setSelectedUsers([...selectedUsers, user]);
+    }
+  };
+
+  function handleCreateGroup() {}
+
+  return (
+    <>
+      <div className={styles.protectOverlay}></div>
+      <div className={styles.group}>
+        <div className={styles.groupHead}>
+          <h2>Create Group Chat</h2>
+          <button>
+            <X
+              size={18}
+              onClick={() => {
+                setIsVisible(false);
+                setSelectedUsers([]);
+              }}
+            />
+          </button>
+        </div>
+        <div className={styles.body}>
+          <input
+            type="text"
+            placeholder="Chat Name"
+            onChange={handleChatName}
+          />
+          <input
+            type="text"
+            placeholder="Add users"
+            onChange={handleSearchUser}
+          />
+          {selectedUsers.length > 0 && (
+            <SelectedUsers
+              selectedUsers={selectedUsers}
+              setSelectedUsers={setSelectedUsers}
+            />
+          )}
+          {userData && (
+            <div className={styles.userContainer}>
+              {userData.map((user) => (
+                <FoundUser
+                  key={user._id}
+                  user={user}
+                  handleAddToSelected={handleAddToSelected}
+                />
+              ))}
+            </div>
+          )}
+          <button className={styles.createBtn} onClick={handleCreateGroup}>
+            Create Chat
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function FoundUser({ user, isActive, handleAddToSelected }) {
+  return (
+    <div
+      className={`${styles.user} ${isActive ? styles.active : ""}`}
+      onClick={() => handleAddToSelected(user)}
+    >
+      <img src={user.pic} alt="" />
+      <div className={styles.userDetails}>
+        <p>{user.name}</p>
+        <p>{user.email}</p>
+      </div>
+    </div>
+  );
+}
+
+function SelectedUsers({
+  selectedUsers,
+  setSelectedUsers = { setSelectedUsers },
+}) {
+  function handleRemoveUser(user) {
+    const newSelectedUsers = selectedUsers.filter(
+      (member) => member.email !== user.email
+    );
+
+    setSelectedUsers(newSelectedUsers);
+  }
+
+  return (
+    <div className={styles.selectedUsers}>
+      {selectedUsers.map((user) => (
+        <div key={user.email} className={styles.selectedUser}>
+          <button onClick={() => handleRemoveUser(user)}>
+            <p>{user.name}</p>
+            <X size={15} />
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default CreateGroup;
---
+++ frontend/src/Components/SingleChat/SingleChat.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+import styles from "./SingleChat.module.css";
+
+function SingleChat({ chat, isActive }) {
+  return (
+    <NavLink
+      className={`${styles.chat}  ${isActive ? styles.active : ""}`}
+      to={`/chats/${chat.users[1]._id}`}
+      key={chat._id}
+    >
+      {chat.latestMessage ? (
+        <>
+          <img src={chat.users[1].pic} alt="" />
+          <h3>{chat.users[1].name} </h3>
+          <p className={styles.author}>{chat.latestMessage}</p> <p>Message</p>
+        </>
+      ) : (
+        <>
+          <img src={chat.users[1].pic} alt="" />
+          <div>
+            <h3>{chat.users[1].name} </h3>
+            <p className={styles.author}>Author :</p> <p>Message</p>
+          </div>
+        </>
+      )}
+    </NavLink>
+  );
+}
+export default SingleChat;
