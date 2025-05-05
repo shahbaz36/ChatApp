1147909acf91228e98531e0ff05f21ddbbc5abd9
Shahbaz Ali <115531577+shahbaz36@users.noreply.github.com>
2025-05-05 20:09:24 +0530

    Add deployed target domain

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3030",
+        target: "https://talksapp.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
